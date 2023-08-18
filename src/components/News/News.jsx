8a522aaf80f60fddb576b71bbbf48b98a5e4d720
OkraGrey <hasnainsohail15@gmail.com>
2023-08-18 20:31:57 +0500

    Add news skeleton loader

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,33 +1,86 @@
+import { useState } from "react";
 import Headline from "../Headline/Headline";
+import { useEffect } from "react";
 
 const News = (props) => {
   // console.log(props);
+  const [newsLoading, setNewsLoading] = useState(true);
   const dateObject = new Date(props.data.publishedAt);
   const formattedDate = dateObject.toISOString().split("T")[0];
+  useEffect(() => {
+    setTimeout(() => {
+      setNewsLoading(false);
+    }, 2000);
+  }, []);
   return props.index == 0 ? (
-    <Headline data={props.data} />
+    <Headline data={props.data} newsLoading={newsLoading} />
   ) : (
-    <div className="lg:mt-4 transition ease-in-out delay-50 duration-300 hover:-translate-y-1 hover:scale-105 mb-2 ">
+    <div
+      className={`lg:mt-4 transition ease-in-out delay-50 duration-300 hover:-translate-y-1 hover:scale-105 mb-2`}
+    >
       <a href={props.data.url} target="_blank" className="inline-block">
-        <div className="sm:w-72 md:w-90 lg:w-72 xl:w-76 desktop:w-80">
-          <div className="overflow-hidden sm:h-60 h-72 md:h-72 sm-h-72 lg:h-44">
-            <img
-              className="rounded-lg w-full h-full object-cover "
-              src={props.data.urlToImage}
-              alt=""
-            />
+        <div
+          className={`${
+            newsLoading && "animate-pulse "
+          } sm:w-72 md:w-90 lg:w-72 xl:w-76 desktop:w-80 `}
+        >
+          <div
+            className={`overflow-hidden sm:h-60 h-72 md:h-72 sm-h-72 lg:h-44`}
+          >
+            {newsLoading ? (
+              <div
+                className={`${
+                  newsLoading
+                    ? "rounded-lg w-full h-full object-cover bg-slate-400"
+                    : ""
+                }`}
+              ></div>
+            ) : (
+              <img
+                className="rounded-lg w-full h-full object-cover "
+                src={props.data.urlToImage}
+                alt=""
+              />
+            )}
           </div>
 
           <div className="text-gray-500/80 text-xs mt-2 mb-1">
-            <p>{formattedDate}</p>
+            {newsLoading ? (
+              <div className={`w-20 h-4 bg-slate-400 rounded-md`}></div>
+            ) : (
+              <p>{formattedDate}</p>
+            )}
           </div>
 
           <div className="text-2xl pb-1 tracking-wide">
-            <h1>{props.data.title}</h1>
+            {newsLoading ? (
+              <>
+                <div
+                  className={`w-full h-9 mb-1 bg-slate-400 rounded-md`}
+                ></div>
+                <div className={`w-full h-12 bg-slate-400 rounded-md`}></div>
+              </>
+            ) : (
+              <h1>{props.data.title}</h1>
+            )}
           </div>
 
           <div className="text-xs text-gray-500 my-2">
-            <p>{props.data.description}</p>
+            {newsLoading ? (
+              <>
+                <div
+                  className={`w-full h-2 mb-1 bg-slate-400 rounded-md`}
+                ></div>
+                <div
+                  className={`w-full h-2 mb-1 bg-slate-400 rounded-md`}
+                ></div>
+                <div
+                  className={`w-full h-2 mb-1 bg-slate-400 rounded-md`}
+                ></div>
+              </>
+            ) : (
+              <p>{props.data.description}</p>
+            )}
           </div>
         </div>
       </a>
